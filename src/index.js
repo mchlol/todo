2923f61b4326b84ff721ecfe92b74b5d5842a256
mchlol <m.andreabr@gmail.com>
2022-10-28 18:58:33 +1100

    add a date handler to convert date in task class

--- src/index.js
+++ src/index.js
@@ -42,12 +42,15 @@ console.log('Huge number: ' + checkDueDate(999999999999999));
 
 // task class
 let taskId = 0; // global variable for incrementing id numbers
+// convert date to human readable format
+const dateHandler = (date) => {
+    return date.toDateString();
+}
 class Task {
-    constructor(title,description,dateAdded,dueDate,priority) {
+    constructor(title,notes,dueDate,priority) {
         this.title = title;
-        this.description = description;
-        this.dateAdded = dateAdded.toDateString();
-        this.dueDate = dueDate.toDateString();
+        this.notes = notes;
+        this.dueDate = dateHandler(dueDate);
         this.priority = priority;
         this.taskId = `task${++taskId}`;
     }
@@ -60,7 +63,8 @@ let someDate = new Date();
 let somePrio = 'Low';
 
 // temp task object
-const someTask = new Task(someTitle,someNotes,someDate,someDate,`${somePrio} priority`);
+const someTask = new Task(someTitle,someNotes,someDate,somePrio);
+const anotherTask = new Task('Wash dishes','',someDate,'Medium');
 
 console.log(someTask);
 
@@ -71,6 +75,7 @@ console.log(someTask);
 const taskList = document.querySelector('#task-list');
 
 function displayTaskItem(object) {
+
     let listItem = document.createElement('li');
     listItem.classList.add('list-group-item', 'task-item-wrapper', 'm-1', 'p-1')
 
@@ -88,9 +93,6 @@ function displayTaskItem(object) {
     title.textContent = object.title;
 
 
-    taskPrimaryWrap.appendChild(check,title);
-
-
     let taskSecondaryWrap = document.createElement('div');
     taskSecondaryWrap.classList.add('task-text-secondary', 'text-muted', 'small');
 
@@ -100,29 +102,44 @@ function displayTaskItem(object) {
     notes.classList.add('m-1','text-justify');
     notes.textContent = object.notes;
 
-
     let detailsRow = document.createElement('div');
     detailsRow.classList.add('d-flex', 'flex-wrap','justify-content-between','align-items-center');
 
-    // date added - don't need to display this for now
-
     let dueDate = document.createElement('span');
-    dueDate.textContent = object.dueDate;
+    dueDate.classList.add('small','m-1');
+    if (!object.dueDate) {
+        dueDate.textContent = 'No due date';
+    }  else dueDate.textContent = `Due ${object.dueDate}`;
 
     let priority = document.createElement('span');
-    priority.textContent = object.prio + ' priority';
+    priority.classList.add('small','m-1');
+    priority.textContent = object.priority + ' priority';
 
     let iconWrap = document.createElement('div');
     iconWrap.classList.add('d-flex');
+
     let editBtn = document.createElement('button');
     editBtn.classList.add('btn','btn-sm');
     editBtn.setAttribute('id','edit');
+    editBtn.innerHTML = `<span class="material-icons text-primary">mode</span>`;
 
+    let delBtn = document.createElement('button');
+    delBtn.classList.add('btn','btn-sm');
+    delBtn.setAttribute('id','delete');
+    delBtn.innerHTML = `<span class="material-icons text-danger">delete</span>`;
 
-    detailsRow.appendChild(dueDate,priority,iconWrap);
+    iconWrap.appendChild(editBtn);
+    iconWrap.appendChild(delBtn);
+
+    detailsRow.appendChild(dueDate);
+    detailsRow.appendChild(priority)
+    detailsRow.appendChild(iconWrap);
 
     taskSecondaryWrap.appendChild(notes);
     taskSecondaryWrap.appendChild(detailsRow);
+
+    taskPrimaryWrap.appendChild(check)
+    taskPrimaryWrap.appendChild(title);
     
     listItem.appendChild(taskPrimaryWrap);
     listItem.appendChild(taskSecondaryWrap);
@@ -133,6 +150,7 @@ function displayTaskItem(object) {
 }
 
 displayTaskItem(someTask);
+displayTaskItem(anotherTask);
 
 /*
 default view is today
