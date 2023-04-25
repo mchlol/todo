a8e9d5817ccb037bf2ddc040257287f1e3997a08
mchlol <m.andreabr@gmail.com>
2023-04-25 14:33:18 +1000

    create a class for tasks

--- src/create.js
+++ src/create.js
@@ -2,6 +2,20 @@ function create() {
     console.log('create things here');
 };
 
+class Task {
+    constructor(title,notes,dueDate,priority,id,completed,project) {
+        this.title = title;
+        this.notes = notes;
+        this.priority = priority;
+        this.id = Date.now(); // unique identifier
+    }
+    toggleCompleted() {
+        this.completed ? true : false;
+    }
+    
+    //methods - toggle completed, set dueDate string value
+}
+
 export {
     create,
 }
